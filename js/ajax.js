c7c72b22c985148b1aedae965ba71887056e1d35
Diego <diego.rv@outlook.com>
2016-04-16 12:13:25 -0500

    Basic styling and search
    Changed api call to search for better use, initial styling and search
    functionality

--- js/ajax.js
+++ js/ajax.js
@@ -1,11 +1,17 @@
 var Ajax = (function() {
 
-	var default_options = {
-		photoset_id: '72157633553599346',
-		photos_per_page: 10
+	var url, onComplete, xhr;
+	var options = {
+		search: '',
+		photos_per_page: 10,
+		page: 1
 	}
-	var url, onComplete;
-	var xhr = new XMLHttpRequest();
+
+	var init = function(){
+		options.search = getDefaultSearch();
+		xhr = new XMLHttpRequest();
+		return this;
+	}
 
 	var call = function() {
 		buildUrl();
@@ -15,7 +21,7 @@ var Ajax = (function() {
 
 	var extendOptions = function(user_options) {
 		for(var key in user_options){
-			default_options[key] = user_options[key];
+			options[key] = user_options[key];
 		}
 		return this;
 	}
@@ -28,7 +34,7 @@ var Ajax = (function() {
 	var doOnComplete = function() {
 		xhr.addEventListener('load', function(e){
 			var obj = JSON.parse(xhr.response);
-			onComplete(obj.photoset);
+			onComplete(obj.photos);
 		});
 	}
 
@@ -39,13 +45,32 @@ var Ajax = (function() {
 	}
 
 	var buildUrl = function(){
-		url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=d1cbdd4b27721ebd877035f83fd52a0d&photoset_id="+default_options.photoset_id+"&per_page="+default_options.photos_per_page+"&format=json&nojsoncallback=1";
+		//url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=d1cbdd4b27721ebd877035f83fd52a0d&photoset_id="+options.photoset_id+"&per_page="+options.photos_per_page+"&format=json&nojsoncallback=1";
+		url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d1cbdd4b27721ebd877035f83fd52a0d&text="+options.search+"&min_taken_date=2015-01-01&sort=relevance&privacy_filter=1&safe_search=1&content_type=1&per_page="+options.photos_per_page+"&page="+options.page+"&format=json&nojsoncallback=1";
+	}
+
+	var getDefaultSearch = function(){
+		var default_search_options = ['Vancouver', 'San Francisco', 'Mexico City'];
+		var option = Math.floor((Math.random() * 3));
+		return default_search_options[option];
+	}
+
+	var getSearch = function(){
+		return options.search;
+	}
+
+	var setSearch = function(search_text){
+		options.search = search_text;
+		return this;
 	}
 
 	return {
+		init: init,
 		options: extendOptions,
 		onLoad: onLoad,
 		onComplete: setOnComplete,
-		call: call
+		call: call,
+		getSearch: getSearch,
+		search: setSearch
 	};
 })();
--- js/main.js
+++ js/main.js
@@ -1,24 +1,53 @@
-window.onload = function() {
+window.onload = function()
+{
+	var ajax = Ajax.init();
 
-	Ajax.options({
-		photos_per_page: 5
-	})
-	.onLoad(function() {
+	ajax.onComplete(function(response)
+	{
+		var search_data = document.getElementById("search_data");
+		var search_text = search_data.querySelectorAll("h2.title span");
+		search_text[0].innerHTML = ajax.getSearch();
 
-		var main_content = document.getElementById("container");
-		main_content.innerHTML = main_content.innerHTML + 'Loading...';
-	})
-	.onComplete(function(response){
-
-		var grid = document.getElementById("photo_grid");
-		var photo_list = response.photo;
-		for(var photo_num in photo_list){
-			
-			var image = document.createElement("IMG");
-			image.src = "https://farm"+photo_list[photo_num].farm+".staticflickr.com/"+photo_list[photo_num].server+"/"+photo_list[photo_num].id+"_"+photo_list[photo_num].secret+"_q.jpg";
-			var node = document.createElement("LI").appendChild(image);
-			grid.appendChild(node);
-		}
+		displayOnGrid(response);
 	})
 	.call();
+
+	document.getElementById("load_more").addEventListener("click", function()
+	{	
+		ajax.search(document.getElementById("search_text").value)
+		.onLoad(function()
+		{
+
+		})
+		.onComplete(function(response)
+		{
+			var search_data = document.getElementById("search_data");
+			var search_text = search_data.querySelectorAll("h2.title span");
+			search_text[0].innerHTML = ajax.getSearch();
+
+			displayOnGrid(response);
+		})
+		.call();
+	});
 };
+
+
+function displayOnGrid(response, append = false)
+{
+	var grid = document.getElementById("photo_grid");
+	if(!append){
+		grid.innerHTML = "";
+	}
+	var photo_list = response.photo;
+	for(var photo_num in photo_list){
+
+		var image = document.createElement("IMG");
+		image.src = "https://farm"+photo_list[photo_num].farm+".staticflickr.com/"+photo_list[photo_num].server+"/"+photo_list[photo_num].id+"_"+photo_list[photo_num].secret+"_q.jpg";
+		
+		var column = document.createElement("DIV");
+		column.className = "thumb col";
+		column.appendChild(image);
+		
+		grid.appendChild(column);
+	}
+}
